7ff52172ae0c3625a0207aedae775880d002191f
Enrique Puyol <kpuyol94@hotmail.com>
2019-10-07 09:35:25 +0200

    Added CreatureTypes

--- Assets/ScriptableObjects/CreatureData.cs
+++ Assets/ScriptableObjects/CreatureData.cs
@@ -4,9 +4,11 @@
 public class CreatureData : ScriptableObject
 {
     public enum MovementTypes { Cruceta, Reina, Diagonal, Salto };
+    public enum CreatureTypes { Naturaleza, Agua, Luz, Oscuridad, Mental, Corrupcion, TOTAL};
 
     public new string name;
 
+    public CreatureTypes creatureType;
     public MovementTypes movementType;
 
     public int movementLimit;
