cca65188a67d79da0b3ddb055da3d4061d9d5142
Enrique Puyol <kpuyol94@hotmail.com>
2019-10-18 15:44:45 +0200

    Added a PieceStats script for the creature´s combat

---
+++ Assets/Scripts/PieceStats.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PieceStats : MonoBehaviour
+{
+    public CreatureData creatureData;
+
+    int currentHealth;
+    int currentMana;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = creatureData.health;
+        currentMana = creatureData.startingMana;
+
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        float h = (float)currentHealth / (float)creatureData.health;
+        float m = (float)currentMana / (float)creatureData.mana;
+
+        GetComponent<uiPiece>().UpdateUI(h, m);
+    }
+
+}
