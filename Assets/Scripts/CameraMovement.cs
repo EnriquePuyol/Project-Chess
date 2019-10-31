438f10db2f4ebb8cfecb1b877003606b1e3e31e8
Enrique Puyol <kpuyol94@hotmail.com>
2019-10-31 18:48:10 +0100

    Created new script to control the camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    bool moving = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+            moving = true;
+        else if (Input.GetMouseButtonUp(1))
+            moving = false;
+
+        if (moving)
+            MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Debug.Log("Mouse X = " + mouseX + ", Mouse Y = " + mouseY);
+    }
+}
